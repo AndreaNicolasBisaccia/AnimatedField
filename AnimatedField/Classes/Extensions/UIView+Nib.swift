504ec508d2365bcf2c9bc164b93a66833da7ed03
Andrea <andrea.bisaccia96@tiscali.it>
2026-08-27 17:11:22 +0200

    Load the xib from the package resource bundle when built with SwiftPM

--- AnimatedField/Classes/Extensions/UIView+Nib.swift
+++ AnimatedField/Classes/Extensions/UIView+Nib.swift
@@ -12,7 +12,12 @@ import UIKit
 extension UIView {
     
     func fromNib<T : UIView>() -> T? {
-        guard let contentView = Bundle(for: type(of: self)).loadNibNamed(String(describing: type(of: self)), owner: self, options: nil)?.first as? T else {
+        #if SWIFT_PACKAGE
+        let bundle = Bundle.module
+        #else
+        let bundle = Bundle(for: type(of: self))
+        #endif
+        guard let contentView = bundle.loadNibNamed(String(describing: type(of: self)), owner: self, options: nil)?.first as? T else {
             // xib not loaded, or its top view is of the wrong type
             return nil
         }
